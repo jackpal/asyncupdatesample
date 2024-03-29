3075c2fac4f65dd5ef2b9c20512dd40e76bfeb24
Jack Palevich <jack.palevich@gmail.com>
2024-03-29 16:10:56 -0700

    Initial Commit

---
+++ asyncupdatesample/asyncupdatesampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncupdatesampleApp.swift
+//  asyncupdatesample
+//
+//  Created by John Palevich on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct asyncupdatesampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
